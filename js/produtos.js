6f0bbd9ce97ad2216bb17a6a43d7c636e7a4b4e6
rian <rianrgomes@gmail.com>
2023-08-01 20:49:43 -0300

    add product cards.

--- js/produtos.js
+++ js/produtos.js
@@ -4,13 +4,12 @@ const compras = document.querySelectorAll('#itens .compra');
 
 compras.forEach((e) =>{
     e.addEventListener('click', () =>{
-
         compras.forEach((compra) =>{
             if(compra!==e){
                 compra.classList.remove('abre');
             }
         })
-       e.classList.add('abre');
+        e.classList.add('abre');
         icone.forEach((i) =>{
             i.addEventListener('click', (event) =>{
                 event.stopPropagation();
